b73e163d161a4d21cd410b50e81155128c7bdec7
User <user@Userde-MacBook-Pro.local>
2019-07-10 19:04:44 +0800

    session not working

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,13 +1,33 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
 const saltRounds = 10
-const app = express();
+
+var cookieParser = require('cookie-parser')
+var session = require('express-session')
+var flash = require('connect-flash');
+var app = express();
+
+app.use(cookieParser('keyboard cat'))
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false, maxAge: 60000 }
+}))
+
+// app.use(function (req, res, next) {
+//     // res.locals.user = req.session.user
+//     // res.locals.success = req.flash('success').toString()
+//     res.locals.msg = req.flash('msg').toString()
+//     next()
+// })
+app.use(flash())
 
 const DB = require('../models/DB_config')
 const Users = DB.ref('/User')
 
 exports.auth = function (req, res) {
-    res.render('auth/index')
+    res.render('auth/index', { messages: req.flash('error') })
 };
 
 exports.login = function (req, res) {
@@ -22,13 +42,15 @@ exports.login = function (req, res) {
             snapshot.forEach(child => userInfo = child.val())
 
             if (!userInfo.email) {
-                // req.flash('info', '無此帳號')
-                res.render('auth/index', { msg: '無此帳號' })
+                req.flash('error', '無此帳號')
+                // res.render('auth/index', { msg: '無此帳號' })
+                res.redirect('back')
             } else {
                 let passwordCompare = await bcrypt.compareSync(password, userInfo.password)
                 if (!passwordCompare) {
-                    // req.flash('info', '密碼錯誤')
-                    res.render('auth/index', { msg: '密碼錯誤' })
+                    req.flash('error', '密碼錯誤')
+                    // res.render('auth/index', { msg: '密碼錯誤' })
+                    res.redirect('back')
                 }
                 res.redirect('/chat')
             }
